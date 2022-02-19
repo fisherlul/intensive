3b8be6da73f1bf25cb687787a57f95bdd9d58676
fisherlul <amazinggumball2301@gmail.com>
2022-02-19 20:46:01 +0700

    Up-to-date

--- Test/index.js
+++ Test/index.js
@@ -1,17 +1,29 @@
-function signup(e){
-    event.preventDefault();
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+import { doc, getDoc,  getFirestore} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js";
 
-    var user = {
-        username = username,
-        password = password,
-    };
-    
-    var json = JSON.stringify(user);
-    localStorage.setItem(user, json);
-}
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwjup3OXcHk7eBaQOXXzaQ10vEr9A2P2k",
+  authDomain: "checkpoint2-95568.firebaseapp.com",
+  projectId: "checkpoint2-95568",
+  storageBucket: "checkpoint2-95568.appspot.com",
+  messagingSenderId: "40658683951",
+  appId: "1:40658683951:web:8e4588d19393fed2ab9eda",
+  measurementId: "G-E05RZZXM18"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const database = getFirestore();
+const Signin = document.getElementById("Signin")
 
 function signin(e){
     event.preventDefault();
